a5ba55c8eee097d5071aed5550e72880df023ad4
Caique <dev.caiquemorales@gmail.com>
2022-03-23 19:17:56 -0300

    empty input issue solved & clear all todos button added

--- src/Todo/index.jsx
+++ src/Todo/index.jsx
@@ -1,24 +1,26 @@
-import { ACTIONS } from '../Todos'
+import { ACTIONS } from "../Todos";
 
 export function Todo({ todo, dispatch }) {
-
-    return (
-
-        <>
-
-            <div className="todo-item">
-                <span className='taref' style={{ color: todo.complete ? "#AAA" : "#000" }}>
-                    {todo.name}
-                </span>
-                <div className="button-area">
-                    <button className='toggle-btn todo-btn not-selectable' onClick={() => dispatch({ type: ACTIONS.TOGGLE_TODO, payload: { id: todo.id } })}>Toggle</button>
-                    <button className='del-btn todo-btn not-selectable' onClick={() => dispatch({ type: ACTIONS.DELETE_TODO, payload: { id: todo.id } })}>Delete</button>
-                </div>
-            </div>
-
-
-        </>
-
-    )
-
+  return (
+    <>
+      <div className="todo-item">
+        <span
+          className="taref"
+          style={{ color: todo.complete ? "#AAA" : "#000" }}
+        >
+          {todo.name}
+        </span>
+        <div className="button-area">
+          <button
+            className="del-btn todo-btn not-selectable"
+            onClick={() =>
+              dispatch({ type: ACTIONS.DELETE_TODO, payload: { id: todo.id } })
+            }
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
